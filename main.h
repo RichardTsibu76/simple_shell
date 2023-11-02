ce7030067258d7206da101ddf4aaf6e91248472b
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-11-02 20:35:31 +0100

    updating the main.h header files in a more suitable format

--- main.h
+++ main.h
@@ -1,13 +1,9 @@
 #ifndef MAIN_H
 #define MIAN_H
-<<<<<<< HEAD
 
 /* These are the list of header files */
-=======
+/* Header for input and output */
 #include <stdio.h>
->>>>>>> e799b160b59701b329edeaf73a84326d8a3ed6d6
-
-
 /* Header files manipulating strings */
 #include <string.h>
 /* Header for unix or other functions or system calls */
