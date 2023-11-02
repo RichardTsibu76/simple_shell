d131f0b5d674e9d4276dfa12951c7d1cbe9ebf2a
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-11-02 20:30:21 +0100

    updating the main.h header files in a more suitable format

--- main.h
+++ main.h
@@ -1,13 +1,22 @@
 #ifndef MAIN_H
 #define MIAN_H
 
+/* These are the list of header files */
 
 
+/* Header for input output */
+#include <stdio.h>
+/* Header files manipulating strings */
+#include <string.h>
+/* Header for unix or other functions or system calls */
+#include <unistd.h>
+/* Header for allocating memory dynamically  for malloc precisely*/
+#include <stdlib.h>
+/* Header files for file controllability */
+#include <fcntl.h>
 
 
-
-
-
+/* THE VARIOUS PROTOTYPES OF THE FUNCTIONS */
 
 
 
