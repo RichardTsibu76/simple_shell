86d1bade90bff710cc0c6a15092071e14c5fc505
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-11-06 16:38:51 +0100

    updating header files

--- main.h
+++ main.h
@@ -12,11 +12,32 @@
 #include <stdlib.h>
 /* Header files for file controllability */
 #include <fcntl.h>
+/* handling system and type definitons for data structures */
+#include <sys/types.h>
+/* For signal handling */ 
+#include <signal.h>
+/* the header for the boolen true or false */
+#include <stdbool.h>
 
+/* THE VARIOUS PROTOTYPES OF THE FUNCTIONS AS PART OF THE SYNOPSIS */
+int put_char(char c);
+void put_prompt(void);
+/* for string duplicate */
 
-/* THE VARIOUS PROTOTYPES OF THE FUNCTIONS */
-
-
-
+char *_strdup(char *str);
+/* for tokenization using delimeter */
+char **break_line(char *buf_tok);
+char *_getenv(char **env, char *name);
+bool put_env(char **env);
+void call_fork(char **argv, char *prog_name, char *full_path, char **env);
+/* This function calculate the len of string */
+int _strlen(char *s);
+/* This function prototypes is for copying of strng from source to destination */
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+/* using the string compare function */
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, int n);
+char *_strchr(char *s, char c);
 
 #endif
